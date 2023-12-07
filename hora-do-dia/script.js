a510d7f76d037ac47394df0a5702aa5b1d0c3f90
michelekluck <140463604+michelekluck@users.noreply.github.com>
2023-12-06 22:19:49 -0300

    ajustar imagem

--- hora-do-dia/script.js
+++ hora-do-dia/script.js
@@ -6,15 +6,15 @@ var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas`
 if (hora >= 0 && hora < 12) { //se a hora for maior ou igual a 0 (meia noite) e a hora for menor que meio dia
     // BOM DIA !
-    img.src = '/hora-do-dia/assets/morning.png'
+    img.src = './assets/morning.png'
     document.body.style.background = '#F5B987'
 } else if (hora >= 12 && hora <= 18) { // se a hora for maior ou igual a meio dia e a menor ou igual a 18 horas
     // BOA TARDE!
-    img.src = '/hora-do-dia/assets/evening.png'
+    img.src = './assets/evening.png'
     document.body.style.background = ' #DCA2C4'
  } else { // outro
     //BOA NOITE!
-    img.src = '/hora-do-dia/assets/night.png'
+    img.src = './assets/night.png'
     document.body.style.background = '#141C32'
  }
 }
